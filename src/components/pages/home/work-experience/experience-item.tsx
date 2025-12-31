cfc4e63c67360a79a87946b421c0c5b2e9ed4b73
Gelzieny R. Martins <gelzieny@gmail.com>
2025-12-31 21:11:54 +0000

    Inserindo data na esperiencia

--- src/components/pages/home/work-experience/experience-item.tsx
+++ src/components/pages/home/work-experience/experience-item.tsx
@@ -1,3 +1,6 @@
+import { ptBR } from 'date-fns/locale'
+import { differenceInMonths, differenceInYears, format } from 'date-fns'
+
 import { RichText } from "@/components/ui/rich-text";
 import type { WorkExperience } from "@/lib/types/page-home";
 import { techDisplayNameMap, techIconMap } from "@/lib/iconMap";
@@ -10,14 +13,38 @@ type Props = {
 };
 
 export function ExperienceItem({ exp, idx }: Props) {
+  const {company, endDate, startDate, companyLogo, companyUrl, role, description, skillsTechnologies} = exp;
+
+  const InitDate = new Date(startDate);
+  const formattedStartDate = format(InitDate, 'MMM yyyy', { locale: ptBR })
+
+  const formattedEndDate = endDate
+    ? format(new Date(endDate), 'MMM yyyy', { locale: ptBR })
+    : 'O momento'
+
+  const end = endDate ? new Date(endDate) : new Date()
+
+  const months = differenceInMonths(end, startDate)
+  const years = differenceInYears(end, startDate)
+  const monthsRemaining = months % 12
+
+  const formattedDuration =
+    years > 0
+      ? `${years} ano${years > 1 ? 's' : ''}${
+          monthsRemaining > 0
+            ? ` e ${monthsRemaining} mes${monthsRemaining > 1 ? 'es' : ''}`
+            : ''
+        }`
+      : `${months} mes${months > 1 ? 'es' : ''}`
+
   return (
     <div className="relative flex gap-6">
       <div className="z-10 flex-shrink-0">
         <div 
           className=" w-14 h-14 rounded-full  flex items-center justify-center text-sm font-medium shadow-md border border-gray-700">
           <img
-            src={exp.companyLogo?.url}
-            alt={exp.company}
+            src={companyLogo?.url}
+            alt={company}
             className="w-12 h-12 rounded-full object-cover"
           />
         </div>
@@ -26,25 +53,28 @@ export function ExperienceItem({ exp, idx }: Props) {
       <div className="flex-1">
         <div className="flex items-center gap-3 ">
           <a 
-            href={exp.companyUrl} 
+            href={companyUrl} 
             target="_blank" 
             className="font-semibold  text-purple-500">
-            {exp.company}
+            {company}
           </a>
         </div>
 
-        <h4 className="mt-1 text-lg font-medium text-gray-500 dark:text-gray-400">{exp.role}</h4>
+        <h4 className="mt-1 text-lg font-medium text-gray-500 dark:text-gray-400">{role}</h4>
+        <span className="text-gray-500">
+          {formattedStartDate} • {formattedEndDate} • ({formattedDuration})
+        </span>
 
-        {exp.description && <RichText content={exp.description.raw} />}
+        {description && <RichText content={description.raw} />}
 
         
 
-        {exp.skillsTechnologies && exp.skillsTechnologies.length > 0 && (
+        {skillsTechnologies && skillsTechnologies.length > 0 && (
           <div className="mt-3">
             <div className="mt-5 text-bold text-gray-500">Competência</div>
 
             <div className="mt-5 flex flex-wrap gap-2">
-              {exp.skillsTechnologies.map((s) => {
+              {skillsTechnologies.map((s) => {
                 const iconKey = techIconMap[s.name] || s.name.toLowerCase();
                 const displayName = techDisplayNameMap[s.name] || s.name;
 
