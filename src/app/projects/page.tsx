a771276a18ba85e34d241cbf7814cd9e11b0abb3
Gelzieny R. Martins <gelzieny@gmail.com>
2026-01-01 18:05:14 +0000

    Adicionando SEO Estático e Dinâmico

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,6 +2,11 @@ import { getProjectPage } from "@/lib/services/getProjectPage";
 import { ProjectsList } from "@/components/pages/Projects/projects-list";
 import { PageIntroduction } from "@/components/pages/Projects/page-introduction";
 
+export const metadata = {
+  title: 'Projetos',
+}
+
+
 export default async function ProjectsPage() {
   const { projects } = await getProjectPage()
 
